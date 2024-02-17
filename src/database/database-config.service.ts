4be5ddaeb132ecee2b158ad7049a9ab231415b3d
Khaled Esmail <khaled.esmail222@gmail.com>
2024-02-18 00:38:32 +0100

    feat:[Jira-7] create a container for the application and the database

--- src/database/database-config.service.ts
+++ src/database/database-config.service.ts
@@ -8,7 +8,7 @@ export class DatabaseConfigService implements TypeOrmOptionsFactory {
   createTypeOrmOptions(): TypeOrmModuleOptions {
     return {
       type: 'postgres',
-      host: process.env.DB_HOST || 'localhost',
+      host: process.env.DB_HOST || 'postgres',
       port: parseInt(process.env.POSTGRES_PORT, 10) || 5432,
       username: process.env.POSTGRES_USER || 'your_username',
       password: process.env.POSTGRES_PASSWORD || 'your_password',
